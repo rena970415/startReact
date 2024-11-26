6be8e80ac2219ab9940fa375b3ef829bb2d0a2cc
강인 이 <gilee@mostisoft.com>
2024-11-26 09:40:24 +0900

    css 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from '@emotion/styled';
+import { keyframes} from '@emotion/react';
+
+const Container = styled.div`
+  text-align:center
+`;
+
+const Header = styled.header`
+  ...
+`;
+
+const spinAnimation = keyframes`
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }    
+`;
+
+const AppLink = styled.a`
+  color:#61afb;
+`;
+
+
+const AppLogo = styled.img`
+  height : 40vmin;
+  pointer-events:none;
+  @media (prefers-reduced-motion: no-preference) { 
+    animation: App-logo-spin infinite 20s linear;   
+  }
+`;
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>      
+      <Header>
+        ...
+      </Header>      
+      <AppLogo src={logo} alt="logo"></AppLogo>
+      <AppLink href="https://reactjs.org"
+        target="blank"
+        rel="nonpener noreferrer"
+      >
+        Learn React
+      </AppLink>
+    </Container>
   );
 }
 
